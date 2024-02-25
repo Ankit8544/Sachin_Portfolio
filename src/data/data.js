ffc4307bc54f84833dbf588c1a3e05b34fa5e0a0
Ankit8544 <ankitkumar875740@gmail.com>
2024-02-25 17:49:37 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,28 @@
+
+
+export const data = [
+  {
+    id: 1,
+    img: 'https://i.postimg.cc/BbH071c5/photo-2023-07-28-23-06-04.jpg',
+    title: "Quad copter drone for payload delivery",
+    desc: "",
+    link: "",
+    git: "https://github.com/Sachin191101/QuadCopterDroneForPayLoadDelivery",
+  },
+  {
+    id: 2,
+    img: 'https://i.postimg.cc/9XRFqDTQ/Price-Pridiction.jpg',
+    title: "Salary Prediction",
+    link: "",
+    git: "https://github.com/Sachin191101/Salary_Prediction",
+  },
+  {
+    id: 3,
+    img: 'https://i.postimg.cc/SNYqj3B2/Agrawal.png',
+    title: "Apartment Management system",
+    desc: "",
+    link: "https://64c50aac42477c6808672544--sunny-dieffenbachia-70af5c.netlify.app",
+    git: "",
+  }
+  
+];
